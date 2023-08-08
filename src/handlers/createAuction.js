838da712ce847726a96f81f6981ad0ade086bef5
LloydJanseVanRensburg <thefullstackjunkie@gmail.com>
2023-08-08 22:33:46 +0200

    initial commit

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,29 @@
+import AWS from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+    const { title } = JSON.parse(event.body);
+
+    const auction = {
+        id: uuid(),
+        title,
+        status: 'OPEN',
+        createdAt: new Date().toISOString(),
+    };
+
+    await dynamodb
+        .put({
+            TableName: process.env.AUCTIONS_TABLE_NAME,
+            Item: auction,
+        })
+        .promise();
+
+    return {
+        statusCode: 201,
+        body: JSON.stringify(auction),
+    };
+}
+
+export const handler = createAuction;
